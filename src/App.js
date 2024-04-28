e7723c4e113e864e03b42fd03ac93149b74dc935
fionav123 <132792253+fionav123@users.noreply.github.com>
2024-04-27 20:22:04 -0400

    message

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import React, { useState } from 'react';
 import ImageSlider from "./components/Slider";
 
 function App() {
-  const images = [ "/images/1.png", 
-  "/images/2.png", 
-  "/images/3.png", 
-  "/images/4.png", 
-  "/images/5.png", 
-  "/images/6.png", 
-  "/images/7.png",
-  "/images/8.png", 
-  "/images/9.png" 
+  const images = [ "1.png", 
+  "2.png", 
+  "3.png", 
+  "4.png", 
+  "5.png", 
+  "6.png", 
+  "7.png",
+  "8.png", 
+  "9.png" 
   ]
 
   return (
